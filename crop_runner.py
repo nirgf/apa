2c630d8ac185fa339c4ad4ade3eb3d4c4775db36
nircko <nir.goldfriend@mail.huji.ac.il>
2024-11-10 14:17:42 +0200

    support option to processes PCI survey + HSI images into segmented pixels and their PCI tag in both options: 1) extract the post-process data into h5 files 2) plot and animate the data

--- crop_runner.py
+++ crop_runner.py
@@ -393,6 +393,33 @@ def read_from_hdf5(file_name):
     for i, (arr, tag) in enumerate(zip(arrays, tags)):
         print(f"Array {i}: shape={arr.shape}, tag={tag}")
     return arrays, tags
+
+
+def create_hdf5_segemets_tags(roi,data_dirname,data_filename,metadata_dirname,metadata_filename, excel_path,masks_tags_bounds):
+    X_cropped, Y_cropped, hys_img, points_merge_PCI, x_new, y_new, coinciding_mask, grid_value, segment_mask = process_spectral_data(
+        roi=roi, data_dirname=data_dirname, data_filename=data_filename, metadata_dirname=metadata_dirname,
+        metadata_filename=metadata_filename, excel_path=excel_path)
+
+    mask_all_channel_values,masks_tags_numerical = create_segments_mask(hys_img, segment_mask,masks_tags_bounds)
+    basename = Path(Path(Path(data_filename).stem).stem).stem + '.h5'
+
+    save_to_hdf5(data_dirname,basename,mask_all_channel_values,masks_tags_numerical)
+
+    pass
+
+def crop_runner_main(roi,data_dirname,data_filename,metadata_dirname,metadata_filename, excel_path):
+    X_cropped, Y_cropped, hys_img, points_merge_PCI, x_new, y_new, coinciding_mask, grid_value, segment_mask = process_spectral_data(
+        roi=roi, data_dirname=data_dirname, data_filename=data_filename, metadata_dirname=metadata_dirname,
+        metadata_filename=metadata_filename, excel_path=excel_path)
+    masks_tags_bounds = (30, 50, 70, 85)  # bounds tags of each segements in format
+    # (segement1_upperbound,segement2_lowerbound,segement2_upperbound,segement3_lowerbound,segement3_upperbound.....,segement_N_lowerbound)
+    mask_all_channel_values, masks_tags_numerical = create_segments_mask(hys_img, segment_mask,masks_tags_bounds)
+    stats_from_mask(mask_all_channel_values, X_cropped, Y_cropped, hys_img, points_merge_PCI, x_new, y_new,
+                    coinciding_mask, grid_value, segment_mask, plot=True, plot_animation=False, dump_json=False)
+
+    pass
+
+
 if __name__ == "__main__":
     # change only these paths or the ROI
     # %% Get venus data
@@ -404,11 +431,15 @@ if __name__ == "__main__":
 
     excel_path = 'seker_nezakim.xls'
 
-    # roi = ((35.095, 35.120), (32.802, 32.818))  # North East Kiryat Ata for train set
-    roi = ((35.064, 35.072), (32.746, 32.754))  # South West Kiryat Ata for test set
+    roi = ((35.095, 35.120), (32.802, 32.818))  # North East Kiryat Ata for train set
+    # roi = ((35.064, 35.072), (32.746, 32.754))  # South West Kiryat Ata for test set
 
+    masks_tags_bounds = (30, 50, 70, 85)  # PCI bounds tags of each segements in format
+    # (segement1_upperbound,segement2_lowerbound,segement2_upperbound,segement3_lowerbound,segement3_upperbound.....,segement_N_lowerbound)
+    create_hdf5_segemets_tags(roi=roi,data_dirname=data_dirname,data_filename=data_filename, metadata_dirname=metadata_dirname,
+                          metadata_filename=metadata_filename,excel_path=excel_path,masks_tags_bounds=masks_tags_bounds)
 
-    process_spectral_data(roi=roi,data_dirname=data_dirname,data_filename=data_filename, metadata_dirname=metadata_dirname,
-                          metadata_filename=metadata_filename,excel_path=excel_path,plot=False, plot_animation=False)
+    # crop_runner_main(roi=roi,data_dirname=data_dirname,data_filename=data_filename, metadata_dirname=metadata_dirname,
+    #                       metadata_filename=metadata_filename,excel_path=excel_path)
 
 
