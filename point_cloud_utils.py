bd9ff29e1580b6011b5784499e73f6fc307185a3
nircko <nir.goldfriend@mail.huji.ac.il>
2024-11-19 14:55:26 +0200

    remove circular import, create a module for plots and debug plotting, refractor crop_runner

--- point_cloud_utils.py
+++ point_cloud_utils.py
@@ -20,8 +20,6 @@ import functools
 from GetRoadsModule import GetRoadsCoordinates
 from sklearn.decomposition import PCA
 
-from crop_runner import nan_arr
-
 # ANSI escape codes for purple text with a black background
 PURPLE_ON_BLACK = "\033[45;30m"
 RESET = "\033[0m"
@@ -49,37 +47,22 @@ def log_execution_time(func):
     return wrapper
 
 
+
+def nan_arr(arr):
+    arr[arr <= 0] = np.nan
+    return arr
+
+
+def or_nan(x1, x2):
+    # Apply "or" operation between the arrays
+    result = np.where(np.isnan(x1) & np.isnan(x2), np.nan,  # If both are NaN, keep NaN
+                      np.where(np.isnan(x1), x2,  # If only x1 is NaN, use x2
+                               np.where(np.isnan(x2), x1,  # If only x2 is NaN, use x1
+                                        x1)))  # If neither is NaN, use x1
+    return result
+
 # Example usage
-def get_lighttraffic_colormap():
-    """
-    Create a colormap that maps low values to red, mid values to yellow,
-    and high values to green, based on the 'jet' colormap.
 
-    Returns:
-    - A customized LinearSegmentedColormap object.
-    """
-    from matplotlib.colors import ListedColormap, BoundaryNorm
-    from matplotlib.colors import LinearSegmentedColormap
-    # # Define custom colors
-    # colors = ['#999999', '#ff0000', '#fbff00', '#32a852']
-    # cmap_me = ListedColormap(colors)
-    # bounds = [-0.5, 0.5, 30.5, 70.5, 100.5]
-    # norm = BoundaryNorm(bounds, cmap_me.N)
-    #
-
-    # Get the 'jet' colormap
-    jet = plt.get_cmap('jet')
-
-    # Extract colors from 0.35 to 0.75 range (red to green)
-    colors = jet(np.linspace(0.5, 1, 256))
-
-    # Flip the colors to map high values to green and low to red
-    colors = colors[::-1]
-
-    # Create a new colormap from the sliced and flipped colors
-    lighttraffic = LinearSegmentedColormap.from_list('lighttraffic', colors)
-
-    return lighttraffic
 
 # Function to categorize a segment based on the closest point's value
 def get_category(x, y, points, PCI):
@@ -255,22 +238,7 @@ def fill_mask_with_irregular_spline(xy_points, X_grid, Y_grid, binary_mask,combi
 
 
 
-# Example usage of fill_line_with_point_values in combination with pcolormesh
-def plot_with_pcolormesh(X_cropped, Y_cropped, Z_cropped, mask_grid):
-    plt.figure(figsize=(8, 6))
 
-    # Plot the original pcolormesh grid
-    plt.pcolormesh(X_cropped, Y_cropped, Z_cropped, cmap='viridis', shading='auto')
-    plt.colorbar(label="Z_cropped values")
-
-    # Overlay the mask using pcolormesh
-    plt.pcolormesh(X_cropped, Y_cropped, mask_grid, cmap='Reds', shading='auto', alpha=0.6)
-    plt.colorbar(label="Mask values")
-
-    plt.title("Mask Overlay on Grid")
-    plt.xlabel("X")
-    plt.ylabel("Y")
-    plt.show()
 
 
 def fill_mask_with_spline(binary_mask,xy_points,combine_mask=False):
@@ -297,50 +265,6 @@ def fill_mask_with_spline(binary_mask,xy_points,combine_mask=False):
     return extended_mask,line_string
 
 
-def scatter_plot_with_annotations(points, ax=None,**kwargs):
-    """
-    Overlay a scatter plot with annotated value dimension on the provided axis.
-
-    Parameters:
-    - points: numpy array of shape (n, 3), where columns represent (x, y, value).
-    - ax: matplotlib axis, optional. If provided, the scatter plot will be drawn on this axis.
-
-    Returns:
-    - ax: The matplotlib axis object with the scatter plot and annotations.
-    """
-    # Extract x, y, and value dimensions
-    x = points[:, 0]
-    y = points[:, 1]
-    values = points[:, 2]
-
-    # If no axis is provided, create a new figure and axis
-    if ax is None:
-        fig, ax = plt.subplots(figsize=(8, 6))
-        ax.grid(True)
-
-    # Scatter plot overlay
-    cmap_me = get_lighttraffic_colormap()
-    cmap_kwargs = kwargs.get('cmap', cmap_me)
-    annotate_flag = kwargs.get('annotate_flag', True)
-    markersize_kwargs = kwargs.get('markersize', 100)
-    markeralpha_kwargs = kwargs.get('alpha', 0.5)
-    linewidths_kwargs = kwargs.get('linewidths', 0.7)
-
-    scatter = ax.scatter(x, y, c=values, s=markersize_kwargs, alpha=markeralpha_kwargs, edgecolor='black',linewidths=linewidths_kwargs,cmap=cmap_kwargs)
-
-    if annotate_flag:
-        # Annotate each point with its rounded value
-        for i in range(len(points)):
-            ax.annotate(f'{int(round(values[i]))}',
-                        (x[i], y[i]),
-                        textcoords="offset points",  # Position text relative to point
-                        xytext=(10, 10),  # Offset the text slightly
-                        ha='center',  # Horizontal alignment
-                        fontsize=10,  # Font size for annotation
-                        bbox=dict(boxstyle="round,pad=0.3", edgecolor='gray', facecolor='white'))
-
-    # Return the axis object with the scatter plot
-    return ax
 
 # -------------------------------
 # Step 3: Segment the Spline by 7-Unit Lengths
