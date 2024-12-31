d276499abc564ff2b9912ced38a87cec61549ef2
Nir <nir.goldfriend@gmail.com>
2024-12-31 17:15:21 +0200

    create a unified image loaded for Kiryat Ata and Detroit

--- src/utils/apa_tester_utils.py
+++ src/utils/apa_tester_utils.py
@@ -75,28 +75,43 @@ def get_GT_xy_PCI(xls_path, isLatLon = False):
     return (lon_vec,lat_vec,pci_vec, seg_id)
 
 @pc_utils.log_execution_time
-def get_hypter_spectral_imaginery(data_filename,data_dirname):
-    bands = range(1, 13)
-    VenusImage_ls = []
-    for b in bands:
-        data_filename = data_filename.split('.')
-        num2replace = len(str(b-1))
-        data_filename[0] = data_filename[0][:-num2replace] + str(b)
-        data_filename = data_filename[0] + '.' + data_filename[1]
-        VenusImage_band = ImportVenusModule.getVenusData(data_dirname, data_filename)
-        VenusImage_ls += [VenusImage_band]
-    VenusImage = np.asarray(VenusImage_ls)
-    VenusImage = np.transpose(VenusImage, axes=(1, 2, 0))
+def get_hypter_spectral_imaginery(data_filename,data_dirname,config_data):
+    if config_data['zone']=="Detroit" and config_data['big_tiff']==False:
+        bands = range(1, 13)
+        VenusImage_ls = []
+        for b in bands:
+            data_filename = data_filename.split('.')
+            num2replace = len(str(b-1))
+            data_filename[0] = data_filename[0][:-num2replace] + str(b)
+            data_filename = data_filename[0] + '.' + data_filename[1]
+            VenusImage_band = ImportVenusModule.getVenusData(data_dirname, data_filename)
+            VenusImage_ls += [VenusImage_band]
+        VenusImage = np.asarray(VenusImage_ls)
+        VenusImage = np.transpose(VenusImage, axes=(1, 2, 0))
 
 
-    #%% Get lat/lon directly from VENUS data - Get kiryatAta only
+        #%% Get lat/lon directly from VENUS data - Get kiryatAta only
 
-    x = getLatLon_fromTiff.convert_raster_to_geocoords(os.path.join(data_dirname,data_filename), zone_number=17, zone_letter='T')
-    # unpack lat/lon
-    lon_mat = x[:, :, 0]
-    lat_mat = x[:, :, 1]
+        x = getLatLon_fromTiff.convert_raster_to_geocoords(os.path.join(data_dirname,data_filename), zone_number=17, zone_letter='T')
+        # unpack lat/lon
+        lon_mat = x[:, :, 0]
+        lat_mat = x[:, :, 1]
+        return lon_mat,lat_mat,VenusImage
 
-    return lon_mat,lat_mat,VenusImage
+    elif config_data['zone']=="Israel" and config_data['big_tiff']==True:
+        VenusImage = ImportVenusModule.getVenusData(data_dirname, data_filename)
+        # %% Get lat/lon directly from VENUS data - Get kiryatAta only
+        x = getLatLon_fromTiff.convert_raster_to_geocoords(data_dirname + data_filename)
+        # unpack lat/lon
+        lon_mat = x[:, :, 0]
+        lat_mat = x[:, :, 1]
+        return lon_mat, lat_mat, VenusImage
+    else:
+        raise Exception("Currently only supports Detroit conversion of coordinates or Israel")
+
+
+
+
 
 def get_PCI_ROI(roi,xy_pci):
     # # example ROI format:
@@ -343,20 +358,26 @@ def equalize_image(img,fill=0):
 
 
 def process_geo_data(config, data_dirname, data_filename, excel_path):
-    lon_mat, lat_mat, VenusImage = get_hypter_spectral_imaginery(data_filename, data_dirname)
+    lon_mat, lat_mat, VenusImage = get_hypter_spectral_imaginery(data_filename, data_dirname,config["data"])
     if "rois" in config["data"]:
         rois = config["data"]["rois"]
         roi = rois[0]
         roi = ((roi[0], roi[1]), (roi[2], roi[3]))
 
     else:
-        roi = (np.min(lat_mat), np.max(lat_mat), (np.min(lon_mat), np.max(lon_mat)))  # Use all data
+        roi = ((np.min(lat_mat), np.max(lat_mat)), (np.min(lon_mat), np.max(lon_mat)))  # Use all data
         rois = [roi]
+    if config["data"]["zone"]=="Israel":
+        GT_xy_PCI = get_GT_xy_PCI(excel_path, isLatLon=False) # convert to Israel Coord to LatLon
+        points_PCI, ROI_point_idx = get_PCI_ROI(roi, GT_xy_PCI[:3])
+        ROI_seg=[]
+    else:
+        GT_xy_PCI = get_GT_xy_PCI(excel_path, isLatLon=True)
+        points_PCI, ROI_point_idx = get_PCI_ROI(roi, GT_xy_PCI[:3])
+        seg_id = GT_xy_PCI[-1]
+        ROI_seg = seg_id[ROI_point_idx]
 
-    GT_xy_PCI = get_GT_xy_PCI(excel_path, isLatLon=True)
-    seg_id = GT_xy_PCI[-1]
-    points_PCI, ROI_point_idx = get_PCI_ROI(roi, GT_xy_PCI[:3])
-    ROI_seg = seg_id[ROI_point_idx]
+
 
     #USE only relevant ROI
     X_cropped, Y_cropped, hys_img,RGB_enchanced, OC_xy = cropROI_Venus_image(roi, lon_mat, lat_mat, VenusImage) # retun also optical center x,y relative to orignal map
@@ -410,93 +431,93 @@ def process_geo_data(config, data_dirname, data_filename, excel_path):
         classified_roads_mask = merge_points_dijkstra
 
     ## Remove of non-gray parts due to incorrect geo-reference
-    for wt in [0.83]:
-        for gyt in [0.1]:
-            for gdt in [0.6,0.2,1.1]:
-                config["preprocessing"]["white_threshold"]=wt
-                config["preprocessing"]["gray_threshold"]=gyt
-                config["preprocessing"]["grad_threshold"]=gdt
-                title_dict={"wt":wt,"gyt":gyt,"gdt":gdt}
+    wt=config["preprocessing"].get("white_threshold", None)
+    gyt = config["preprocessing"].get("gray_threshold", None)
+    gdt = config["preprocessing"].get("grad_threshold", None)
+    if any(x is None for x in (wt, gyt, gdt)):
+        segment_mask=classified_roads_mask
+    else:
+        title_dict={"wt":wt,"gyt":gyt,"gdt":gdt}
 
-                # TODO: optimize all the parameters of enhancment
-                enhance_morph_operator_type=config["preprocessing"].get("enhance_morph_operator_type", "dilation")
-                enhance_morph_operator_size=config["preprocessing"].get("enhance_morph_operator_size", 3)
-                if enhance_morph_operator_type is not None and enhance_morph_operator_size>0:
-                    segment_mask_nan = pc_utils.nan_arr(
-                        pc_utils.morphological_operator_multiclass_mask(classified_roads_mask,enhance_morph_operator_type, 'square', enhance_morph_operator_size))
-                else:
-                    segment_mask_nan = pc_utils.nan_arr(classified_roads_mask)
-                gray_color_enhanced=enhance_gray_based_on_RGB(config,RGB_enchanced,segment_mask_nan)
-                # combine_mask_roads = pc_utils.morphological_operator_multiclass_mask(gray_color_enhanced, 'closing', 'square', 1)
-                segment_mask=pc_utils.nan_arr(gray_color_enhanced)
+        # TODO: optimize all the parameters of enhancment
+        enhance_morph_operator_type=config["preprocessing"].get("enhance_morph_operator_type", "dilation")
+        enhance_morph_operator_size=config["preprocessing"].get("enhance_morph_operator_size", 3)
+        if enhance_morph_operator_type is not None and enhance_morph_operator_size>0:
+            segment_mask_nan = pc_utils.nan_arr(
+                pc_utils.morphological_operator_multiclass_mask(classified_roads_mask,enhance_morph_operator_type, 'square', enhance_morph_operator_size))
+        else:
+            segment_mask_nan = pc_utils.nan_arr(classified_roads_mask)
+        gray_color_enhanced=enhance_gray_based_on_RGB(config,RGB_enchanced,segment_mask_nan)
+        # combine_mask_roads = pc_utils.morphological_operator_multiclass_mask(gray_color_enhanced, 'closing', 'square', 1)
+        segment_mask=pc_utils.nan_arr(gray_color_enhanced)
 
 
-                # Remove of outliers based on object detection on the RGB image
-                Y , _ , _ = pc_utils.rgb_to_yuv(RGB_enchanced)
-                grad_threshold = config["preprocessing"].get("grad_threshold", 0.6)
+        # Remove of outliers based on object detection on the RGB image
+        Y , _ , _ = pc_utils.rgb_to_yuv(RGB_enchanced)
+        grad_threshold = config["preprocessing"].get("grad_threshold", 0.6)
 
-                # do sobel over mean filter luminace visible image
-                _ , _ , mag = pc_utils.sobel_gradient(pc_utils.mean_filter(Y,3)) # do sobel over mean filter luminace visible image
+        # do sobel over mean filter luminace visible image
+        _ , _ , mag = pc_utils.sobel_gradient(pc_utils.mean_filter(Y,3)) # do sobel over mean filter luminace visible image
 
-                # naive object detection using threshold over graident image, so find in region where there is a valid PCI mask where the grad of the RGB image is larger than defined threshold
-                objects_detected_im_mask = np.where(pc_utils.morphological_operator_multiclass_mask(classified_roads_mask, 'dilation', 'square', 3) > 0, mag, 0) > grad_threshold
-                segment_mask_obj_removed = np.where(objects_detected_im_mask,np.nan ,segment_mask)
-                print(f'After object detection, Not nan pixels in new mask {np.sum(~np.isnan(segment_mask_obj_removed))} pixels\nRemoved {100*(1-np.sum(~np.isnan(segment_mask_obj_removed))/np.sum(~np.isnan(segment_mask))):.2f}% of pixels from original mask\n\n')
-                debug_features_engineering=False
-                if debug_features_engineering:
-                    channel_of_intreset=12
-                    array=pc_utils.mean_filter(hys_img[:,:,channel_of_intreset-1],3)
-                    debug_seg_id=3
-                    debug_reflectivity_thrshld=0.22 # gray level thrshold
-                    exceeding_areas = (array > debug_reflectivity_thrshld) & (classified_roads_mask == debug_seg_id)
+        # naive object detection using threshold over graident image, so find in region where there is a valid PCI mask where the grad of the RGB image is larger than defined threshold
+        objects_detected_im_mask = np.where(pc_utils.morphological_operator_multiclass_mask(classified_roads_mask, 'dilation', 'square', 3) > 0, mag, 0) > grad_threshold
+        segment_mask_obj_removed = np.where(objects_detected_im_mask,np.nan ,segment_mask)
+        print(f'After object detection, Not nan pixels in new mask {np.sum(~np.isnan(segment_mask_obj_removed))} pixels\nRemoved {100*(1-np.sum(~np.isnan(segment_mask_obj_removed))/np.sum(~np.isnan(segment_mask))):.2f}% of pixels from original mask\n\n')
+        debug_features_engineering=False
+        if debug_features_engineering:
+            channel_of_intreset=12
+            array=pc_utils.mean_filter(hys_img[:,:,channel_of_intreset-1],3)
+            debug_seg_id=3
+            debug_reflectivity_thrshld=0.22 # gray level thrshold
+            exceeding_areas = (array > debug_reflectivity_thrshld) & (classified_roads_mask == debug_seg_id)
 
-                    import src.utils.pc_plot_utils as plt_utils
+            import src.utils.pc_plot_utils as plt_utils
 
-                    # plt.figure(figsize=(8, 8))
-                    # plt.imshow(RGB_enchanced, cmap='gray')
-                    # plt.colorbar()
-                    # plt.imshow(pc_utils.nan_arr(pc_utils.dilate_mask(exceeding_areas,30).astype('float')),
-                    #            cmap='Reds', alpha=0.5, label='outliers_pixels')
-                    # plt.title(f'Geo-registration problem seg:{debug_seg_id}\nchannel12_trhsold_gl{debug_reflectivity_thrshld}')
-                    #
-                    plt.figure(figsize=(8, 8))
-                    plt.imshow(RGB_enchanced, cmap='gray')
-                    plt.colorbar()
-                    plt.imshow(pc_utils.nan_arr(segment_mask_obj_removed.astype('float')), cmap=plt_utils.get_lighttraffic_colormap(), alpha=0.5,label='outliers_pixels')
-                    plt.title(f'Mask after enhancement:{title_dict}')
-                    #
-                    #
-                    # plt.figure(figsize=(12, 6))
-                    # plt.subplot(1, 2, 1)
-                    # plt.pcolormesh(X_cropped, Y_cropped, RGB_enchanced)
-                    #
-                    # plt.subplot(1, 2, 2)
-                    # plt.pcolormesh(X_cropped, Y_cropped, Y,cmap='gray')
-                    # plt.pcolormesh(X_cropped,Y_cropped,segment_mask_obj_removed,cmap=plt_utils.get_lighttraffic_colormap())
-                    # # nonzero_indices = np.nonzero(np.nan_to_num(segment_mask_obj_removed,0))
-                    # # nonzero_indices_sparse=nonzero_indices[::10]
-                    # # values = segment_mask_obj_removed[nonzero_indices_sparse]
-                    # # plt.scatter(
-                    # #     X_cropped[nonzero_indices_sparse], Y_cropped[nonzero_indices_sparse],
-                    # #     c=values,
-                    # #     s=20,    # Marker size
-                    # #     marker="o", # Circle marker
-                    # #     edgecolor="black",  # Marker edge color
-                    # #     linewidths=0.05  # Marker edge width
-                    # # )
-                    # plt.scatter(
-                    #     points_PCI[:,0], points_PCI[:,1],
-                    #     c=points_PCI[:,2],
-                    #     cmap=plt_utils.get_lighttraffic_colormap(),
-                    #     s=50,    # Marker size
-                    #     marker="*",  # Circle marker
-                    #     edgecolor="black",  # Marker edge color
-                    #     linewidths=0.5  # Marker edge width
-                    # )
-                    stat_from_segments = analyze_pixel_value_ranges(hys_img, segment_mask_obj_removed,[1,2,3])
-                    wavelengths_array = 1e-3 * np.array([info['wavelength'] for info in bands_dict.values()])
-                    plt_utils.plot_spectral_curves(wavelengths_array, stat_from_segments,None,title=title_dict | {"divide with gray level channel":3})
-                    pc_utils.apply_masks_and_average(hys_img, segment_mask_obj_removed == 3, debug_plots=True,suptitle=title_dict | {"pci=":3})
+            # plt.figure(figsize=(8, 8))
+            # plt.imshow(RGB_enchanced, cmap='gray')
+            # plt.colorbar()
+            # plt.imshow(pc_utils.nan_arr(pc_utils.dilate_mask(exceeding_areas,30).astype('float')),
+            #            cmap='Reds', alpha=0.5, label='outliers_pixels')
+            # plt.title(f'Geo-registration problem seg:{debug_seg_id}\nchannel12_trhsold_gl{debug_reflectivity_thrshld}')
+            #
+            plt.figure(figsize=(8, 8))
+            plt.imshow(RGB_enchanced, cmap='gray')
+            plt.colorbar()
+            plt.imshow(pc_utils.nan_arr(segment_mask_obj_removed.astype('float')), cmap=plt_utils.get_lighttraffic_colormap(), alpha=0.5,label='outliers_pixels')
+            plt.title(f'Mask after enhancement:{title_dict}')
+            #
+            #
+            # plt.figure(figsize=(12, 6))
+            # plt.subplot(1, 2, 1)
+            # plt.pcolormesh(X_cropped, Y_cropped, RGB_enchanced)
+            #
+            # plt.subplot(1, 2, 2)
+            # plt.pcolormesh(X_cropped, Y_cropped, Y,cmap='gray')
+            # plt.pcolormesh(X_cropped,Y_cropped,segment_mask_obj_removed,cmap=plt_utils.get_lighttraffic_colormap())
+            # # nonzero_indices = np.nonzero(np.nan_to_num(segment_mask_obj_removed,0))
+            # # nonzero_indices_sparse=nonzero_indices[::10]
+            # # values = segment_mask_obj_removed[nonzero_indices_sparse]
+            # # plt.scatter(
+            # #     X_cropped[nonzero_indices_sparse], Y_cropped[nonzero_indices_sparse],
+            # #     c=values,
+            # #     s=20,    # Marker size
+            # #     marker="o", # Circle marker
+            # #     edgecolor="black",  # Marker edge color
+            # #     linewidths=0.05  # Marker edge width
+            # # )
+            # plt.scatter(
+            #     points_PCI[:,0], points_PCI[:,1],
+            #     c=points_PCI[:,2],
+            #     cmap=plt_utils.get_lighttraffic_colormap(),
+            #     s=50,    # Marker size
+            #     marker="*",  # Circle marker
+            #     edgecolor="black",  # Marker edge color
+            #     linewidths=0.5  # Marker edge width
+            # )
+            stat_from_segments = analyze_pixel_value_ranges(hys_img, segment_mask_obj_removed,[1,2,3])
+            wavelengths_array = 1e-3 * np.array([info['wavelength'] for info in bands_dict.values()])
+            plt_utils.plot_spectral_curves(wavelengths_array, stat_from_segments,None,title=title_dict | {"divide with gray level channel":3})
+            pc_utils.apply_masks_and_average(hys_img, segment_mask_obj_removed == 3, debug_plots=True,suptitle=title_dict | {"pci=":3})
 
     return X_cropped, Y_cropped, hys_img, points_merge_PCI, coinciding_mask, segment_mask
 
