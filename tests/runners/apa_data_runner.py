031b25fdd406a5fb1efa6260e7ae92b900cd3232
Nir <nir.goldfriend@gmail.com>
2025-01-02 12:14:09 +0200

    iterate and save file based on ROI over all ROIs from config list

--- tests/runners/apa_data_runner.py
+++ tests/runners/apa_data_runner.py
@@ -23,7 +23,7 @@ print(REPO_ROOT)
 
 
 #%% Generate Database For NN
-def create_database_from_VENUS(config_path,data_dirname,data_filename,metadata_filename, excel_path,output_path=None,debug_plots=True):
+def create_database_from_VENUS(config_path,data_dirname,data_filename,metadata_filename, excel_path,output_dirname=None,debug_plots=True):
     ### Get data and prepare it for NN ###
 
     # Read config and merge it with default values of configs if some keys are missing
@@ -47,53 +47,53 @@ def create_database_from_VENUS(config_path,data_dirname,data_filename,metadata_f
             filled_with = np.nan_to_num(segment_mask, nan=mask_null_fill_value)
             boudningbox_list_labeled_image = pc_utils.process_labeled_image(hys_img, segment_mask, segID_PCI_LUT,
                                                                             dilation_radius=1)
-
             replaced_mask = np.vectorize(key_to_int_map.get)(filled_with.astype('int'))
-
             # replace nan/None/null with some other value
             segment_mask = np.where(replaced_mask == None, mask_null_fill_value, replaced_mask).astype('int')
 
-    stat_from_segments = apa_utils.analyze_pixel_value_ranges(hys_img, segment_mask)
-    wavelengths_array = 1e-3 * np.array([info['wavelength'] for info in bands_dict.values()])
-    if debug_plots:
-        plt_utils.plot_spectral_curves(wavelengths_array, stat_from_segments,None,'mean')
-    binary_seg_mask = (segment_mask > 0)*1
-    road_hys_filter = np.reshape(binary_seg_mask, list(np.shape(segment_mask)) + [1])
+        stat_from_segments = apa_utils.analyze_pixel_value_ranges(hys_img, segment_mask)
+        wavelengths_array = 1e-3 * np.array([info['wavelength'] for info in bands_dict.values()])
+        if debug_plots:
+            plt_utils.plot_spectral_curves(wavelengths_array, stat_from_segments,None,'mean')
+        binary_seg_mask = (segment_mask > 0)*1
+        road_hys_filter = np.reshape(binary_seg_mask, list(np.shape(segment_mask)) + [1])
 
-    # Gets the roads in general
-    # crop_size=config['preprocessing']['augmentations']['crop_size'][0]
-    num_of_channels = np.shape(hys_img)[-1]
-    hys_roads = np.repeat(road_hys_filter, num_of_channels, -1)*hys_img
-    NN_inputs = pp.crop_image_to_segments(config,hys_roads, image_dim=num_of_channels)
-    NN_inputs[np.isnan(NN_inputs)] = 0
+        # Gets the roads in general
+        # crop_size=config['preprocessing']['augmentations']['crop_size'][0]
+        num_of_channels = np.shape(hys_img)[-1]
+        hys_roads = np.repeat(road_hys_filter, num_of_channels, -1)*hys_img
+        NN_inputs = pp.crop_image_to_segments(config,hys_roads, image_dim=num_of_channels)
+        NN_inputs[np.isnan(NN_inputs)] = 0
 
-    # TODO: validate config values and add output path to functions that outputs files
-    # Gets only the labeled roads
-    labeled_road_mask = np.ones(np.shape(binary_seg_mask))
-    labeled_road_mask[np.isnan(segment_mask)] = 0
-    labeled_road_mask = np.reshape(labeled_road_mask*binary_seg_mask, list(np.shape(labeled_road_mask)) + [1])
-    hys_labeled_roads = np.repeat(labeled_road_mask, num_of_channels, -1)*hys_img
-    NN_labeled_inputs = pp.crop_image_to_segments(config,hys_labeled_roads, image_dim=num_of_channels)
-    NN_labeled_inputs[np.isnan(NN_labeled_inputs)] = 0
-    true_labels_full_image = np.reshape(segment_mask, list(np.shape(segment_mask)) + [1]) * labeled_road_mask
-    true_labels_full_image[np.isnan(true_labels_full_image)] = 0
-    true_labels = pp.crop_image_to_segments(config,true_labels_full_image, image_dim=1)
-    
-    # Remove frames with zeros only
-    non_zero_idx = np.argwhere(np.sum(np.sum(np.sum(true_labels, -1), -1), -1) > 0)
-    fin_NN_inputs = NN_inputs[non_zero_idx[:, 0], :, :, :]
-    fin_true_labels = true_labels[non_zero_idx[:, 0], :, :, :]
-    fin_NN_labeled_inputs = NN_labeled_inputs[non_zero_idx[:, 0], :, :, :]
-    # TODO: validate config values and add output path to functions that outputs files
+        # TODO: validate config values and add output path to functions that outputs files
+        # Gets only the labeled roads
+        labeled_road_mask = np.ones(np.shape(binary_seg_mask))
+        labeled_road_mask[np.isnan(segment_mask)] = 0
+        labeled_road_mask = np.reshape(labeled_road_mask*binary_seg_mask, list(np.shape(labeled_road_mask)) + [1])
+        hys_labeled_roads = np.repeat(labeled_road_mask, num_of_channels, -1)*hys_img
+        NN_labeled_inputs = pp.crop_image_to_segments(config,hys_labeled_roads, image_dim=num_of_channels)
+        NN_labeled_inputs[np.isnan(NN_labeled_inputs)] = 0
+        true_labels_full_image = np.reshape(segment_mask, list(np.shape(segment_mask)) + [1]) * labeled_road_mask
+        true_labels_full_image[np.isnan(true_labels_full_image)] = 0
+        true_labels = pp.crop_image_to_segments(config,true_labels_full_image, image_dim=1)
 
-    ### Save the data ###
-    pp.save_cropped_segments_to_h5(fin_NN_inputs, os.path.join(output_path,'All_RoadVenus.h5'))
-    pp.save_cropped_segments_to_h5(fin_true_labels, os.path.join(output_path,'PCI_labels.h5'))
-    pp.save_cropped_segments_to_h5(fin_NN_labeled_inputs, os.path.join(output_path,'Labeld_RoadsVenus.h5'))
-    basename = Path(Path(Path(data_filename).stem).stem).stem + '.h5'
-    print(basename)
+        # Remove frames with zeros only
+        non_zero_idx = np.argwhere(np.sum(np.sum(np.sum(true_labels, -1), -1), -1) > 0)
+        fin_NN_inputs = NN_inputs[non_zero_idx[:, 0], :, :, :]
+        fin_true_labels = true_labels[non_zero_idx[:, 0], :, :, :]
+        fin_NN_labeled_inputs = NN_labeled_inputs[non_zero_idx[:, 0], :, :, :]
+        # TODO: validate config values and add output path to functions that outputs files
+        ### Save the data ###
+        output_path = Path(output_dirname)
+        base_files = ["All_RoadVenus", "PCI_labels", "Labeld_RoadsVenus","BoudingBoxList"]
+        formatted_string = "_".join(map(lambda x: str(round(x)), roi))
+        pp.save_cropped_segments_to_h5(fin_NN_inputs, output_path / f"All_RoadVenus_{formatted_string}.h5")
+        pp.save_cropped_segments_to_h5(fin_true_labels, output_path / f"PCI_labels_{formatted_string}.h5")
+        pp.save_cropped_segments_to_h5(fin_NN_labeled_inputs, output_path / f"Labeld_RoadsVenus_{formatted_string}.h5")
+        basename = Path(Path(Path(data_filename).stem).stem).stem + '.h5'
+        print(basename)
 
-
+    print('Done.')
 
 
 if __name__ == "__main__":
